9b690be106e1787bc5e2f7d51a781d8f131b4671
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-07 10:00:53 -0700

    C:/Program Files/Git/data/country/id endpoint now functional

--- gdpApp/src/main/java/local/alfonso/gdp/controller/GDPController.java
+++ gdpApp/src/main/java/local/alfonso/gdp/controller/GDPController.java
@@ -4,12 +4,14 @@ package local.alfonso.gdp.controller;
 import local.alfonso.gdp.GDPComp;
 import local.alfonso.gdp.GDPList;
 import local.alfonso.gdp.GdpApplication;
+import local.alfonso.gdp.exception.ResourceNotFoundException;
 import local.alfonso.gdp.model.GDP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +44,21 @@ public class GDPController {
        return new ResponseEntity<>(GdpApplication.ourGdpList.gdpList, HttpStatus.OK);
     }
 
+//    localhost:2019/data/{id}
+    @GetMapping(value = "/country/{id}", produces = {"application/json"})
+    public ResponseEntity<?> getCountry(HttpServletRequest req, @PathVariable long id){
+        logger.info(req.getRequestURI() + " accessed");
+
+        GDP rtnGdp;
+        if(GdpApplication.ourGdpList.findGDP(g -> (g.getId() == id)) == null )
+        {
+            throw new ResourceNotFoundException("Country with id " + id + " not found.");
+        } else
+        {
+            rtnGdp = GdpApplication.ourGdpList.findGDP(g -> (g.getId() == id));
+        }
+
+        return new ResponseEntity<>(rtnGdp, HttpStatus.OK);
+    }
 
 }
