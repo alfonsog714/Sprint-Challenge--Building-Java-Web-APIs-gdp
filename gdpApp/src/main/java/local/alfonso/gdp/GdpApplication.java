4feb4437de0b45a5853ce59e112f5732f4ac641e
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-07 09:25:01 -0700

    Added ResourceNotFoundException, RestException and GDPlist class. Also enabled Mvc on application

--- gdpApp/src/main/java/local/alfonso/gdp/GdpApplication.java
+++ gdpApp/src/main/java/local/alfonso/gdp/GdpApplication.java
@@ -2,12 +2,24 @@ package local.alfonso.gdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class GdpApplication {
+@EnableWebMvc
+public class GdpApplication
+{
+    public static GDPList ourGdpList;
 
-    public static void main(String[] args) {
-        SpringApplication.run(GdpApplication.class, args);
+    public static void main(String[] args)
+    {
+        ourGdpList = new GDPList();
+
+        ApplicationContext ctx = SpringApplication.run(GdpApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
