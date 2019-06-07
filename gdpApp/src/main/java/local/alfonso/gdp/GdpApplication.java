a24099c7463b934917d7c8dcc85e61d4c29ac798
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-07 08:58:07 -0700

    Initial commit

---
+++ gdpApp/src/main/java/local/alfonso/gdp/GdpApplication.java
@@ -0,0 +1,13 @@
+package local.alfonso.gdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdpApplication.class, args);
+    }
+
+}
